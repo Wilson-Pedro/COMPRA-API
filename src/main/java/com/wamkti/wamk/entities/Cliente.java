0770c184aee6a2363c51d468b93504fcf330c970
Wilson-Pedro <wilsonpedroneto@gmail.com>
2023-05-26 14:55:35 -0300

    Conexão com  banco de dados H2 e criação da Entidade Cliente

---
+++ src/main/java/com/wamkti/wamk/entities/Cliente.java
@@ -0,0 +1,72 @@
+package com.wamkti.wamk.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_cliente")
+public class Cliente implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private Double dinheiro;
+	
+	public Cliente() {
+	}
+
+	public Cliente(Long id, String nome, Double dinheiro) {
+		this.id = id;
+		this.nome = nome;
+		this.dinheiro = dinheiro;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getDinheiro() {
+		return dinheiro;
+	}
+
+	public void setDinheiro(Double dinheiro) {
+		this.dinheiro = dinheiro;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+}
