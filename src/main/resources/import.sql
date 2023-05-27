1a29dae16eb689169d95b593cbf0c846d62a377a
Wilson-Pedro <wilsonpedroneto@gmail.com>
2023-05-27 07:11:11 -0300

    Inserindo valores na table produto

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,8 @@
 INSERT INTO tbl_cliente (nome, dinheiro) VALUES ('Wilson Pedro', 5000.0);
 INSERT INTO tbl_cliente (nome, dinheiro) VALUES ('Ana Julia', 7000.0);
 INSERT INTO tbl_cliente (nome, dinheiro) VALUES ('Julio Cezar', 4500.0);
+
+
+INSERT INTO tbl_produto (nome_produto, quantidade, preco) VALUES ('Relógio', 1, 20.0);
+INSERT INTO tbl_produto (nome_produto, quantidade, preco) VALUES ('Celular', 1, 2500.0);
+INSERT INTO tbl_produto (nome_produto, quantidade, preco) VALUES ('Livro', 5, 850.0);
